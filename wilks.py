3ed4ae535c07db4e6a2fb026039f558903c338d7
Robert Ross Wardrup <minorsecond@gmail.com>
2015-06-20 16:18:27 -0500

    Added some interesting statistics to output of compare().

--- wilks.py
+++ wilks.py
@@ -36,7 +36,7 @@ def coef(units, sex, weight):
     w_coef = 0
     coefs = dict.fromkeys(['a', 'b', 'c', 'd', 'e', 'f'], 0)
 
-    if sex == 'male':
+    if sex == 'M':
         coefs['a'] = -216.0475144
         coefs['b'] = 16.2606339
         coefs['c'] = -0.002388645
@@ -44,7 +44,7 @@ def coef(units, sex, weight):
         coefs['e'] = 0.00000701863
         coefs['f'] = -0.00000001291
 
-    elif sex == 'female':
+    elif sex == 'F':
         coefs['a'] = 594.31747775582
         coefs['b'] = -27.23842536447
         coefs['c'] = 0.82112226871
@@ -79,7 +79,7 @@ def wilkscore(units, total, coef):
     return score
 
 
-def filter(csvpath):
+def filter(csvpath, sex, weight):
     """
     parse powerlifting results based on sex, gear, age, etc.
     Uses output from https://github.com/sstangl/openpowerlifting
@@ -94,12 +94,13 @@ def filter(csvpath):
     with open(csvpath, "rt") as csvfile:
         datareader = csv.reader(csvfile)
         count = 0
-        sex = "M"
-        wclass = "74.84"
+        wclass = ["74.84"]
+        #wclass = ['59.87', '60', '59', '57']
+        equip = "Raw"
         wilks = []
         for row in datareader:
             if row[15]:
-                if row[3] == "{0}".format(sex) and row[5] == "{0}".format(wclass):
+                if row[3] == "{0}".format(sex) and row[4] == "{0}".format(equip) and row[5] in wclass:
                     w = float(row[15])
                     wilks.append(w)
                     count += 1
@@ -118,7 +119,9 @@ def mean_ci(data, confidence=0.95):
     m, se = np.mean(a), scipy.stats.sem(a)
     h = se * sp.stats.t._ppf((1 + confidence) / 2., n-1)
 
-    return m, m-h, m+h
+    p25, p50, p75 = np.percentile(data, 25), np.percentile(data, 50), np.percentile(data, 75)
+
+    return m, m-h, m+h, p25, p50, p75
 
 
 def main_menu():
@@ -129,11 +132,46 @@ def main_menu():
     answer = int(input(">>> "))
     if answer == 1:
         calc()
+
     elif answer == 2:
-        w = input(("Enter your Wilk's score: "))
-        wilks = filter(csvpath)
-        mean, lc, hc = mean_ci(wilks)
-        print()
+        print("1. Male\n"
+              "2. Female")
+        sex = int(input(">>> "))
+        if sex == 1:
+            sex = 'M'
+        elif sex == 2:
+            sex = 'F'
+
+        weight = float(input("\nEnter weight in lbs: "))
+
+        total = float(input(("\nEnter total weight lifted in lbs: ")))
+
+        score = round(wilkscore('lbs', total, coef('lbs', sex, weight)), 2)
+        wilks = filter(csvpath, sex, weight)
+        athletes = len(wilks)
+        mean, lc, hc, p25, p50, p75 = mean_ci(wilks)
+        print("\nMean Wilk's Score: {0}, out of {1} observations.".format(round(mean, 2), athletes))
+        print("The 95% confidence interval lies between {0} and {1}".format(round(lc, 2), round(hc, 2)))
+        print("Median Wilk's Score: {0}".format(round(p50, 2)))
+        print("The upper and lower quartiles are {0} and {1}".format(p25, p75))
+        print("\n\nYour Wilk's score is {0}".format(score))
+        score = int(score)
+        lc = int(lc)
+        hc = int(hc)
+        p25, p50, p75 = int(p25), int(p50), int(p75)
+        if score in range(lc, hc):
+            print("Your score, {0}, is within the confidence interval of {1} to {2} points.".format(score, lc, hc))
+        elif score in range(hc, 99999):
+            print("Your score, {0}, is above the confidence interval {1} points. You should compete!".format(score, hc))
+        if score in range(p25, p50):
+            print("Your score is in the 25th percentile of lifts, {0} to {1} points.".format(p25, p50))
+        elif score in range(p50, p75):
+            print("Your score is in the 75th percentile of lifts, {0} to {1} points. You should compete!"
+                  .format(p50, p75))
+
+        input("\nPress enter to return to main menu")
+        main_menu()
+
     else:
         main_menu()
 
